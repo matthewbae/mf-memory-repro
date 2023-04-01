ab75fdf8fcc05d29e3556524366acdcb27e92de5
Matt Bae <mbae@roblox.com>
2023-04-01 13:53:13 -0700

    different app

---
+++ react-remote/src/components/App.tsx
@@ -0,0 +1,14 @@
+import LocalButton from './Button';
+import React from 'react';
+import { AccessAlarmOutlined } from "@mui/icons-material";
+
+const App = () => (
+  <div>
+    <h1>Basic Host-Remote</h1>
+    <h2>App 2</h2>
+    <LocalButton />
+    <AccessAlarmOutlined></AccessAlarmOutlined>
+  </div>
+);
+
+export default App;
